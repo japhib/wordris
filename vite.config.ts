87e9982c20909f75adf13136983de09d29aaa08a
Jan Bergeson <jphbergeson@gmail.com>
2022-11-29 16:37:01 -0700

    try fix url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wordris/',
   build: {
     // output to `docs` for use with Github Pages
     outDir: 'docs'
